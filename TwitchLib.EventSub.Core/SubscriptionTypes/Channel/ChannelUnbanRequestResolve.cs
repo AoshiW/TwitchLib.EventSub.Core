e4ba137c0cbbcce202c5139f7c7801e02d6a184b
AoshiW <ondrula98@gmail.com>
2024-04-18 12:34:56 +0200

    Add `Channel Unban Request *` types

---
+++ TwitchLib.EventSub.Core/SubscriptionTypes/Channel/ChannelUnbanRequestResolve.cs
@@ -0,0 +1,59 @@
+﻿namespace TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
+
+/// <summary>
+/// Channel Unban Request Resolve subscription type model
+/// <para>Description:</para>
+/// <para>An unban request has been resolved.</para>
+/// </summary>
+public sealed class ChannelUnbanRequestResolve 
+{
+    /// <summary>
+    /// The ID of the unban request.
+    /// </summary>
+    public string Id { get; set; } = string.Empty;
+    /// <summary>
+    /// The broadcaster’s user ID for the channel the unban request was updated for.
+    /// </summary>
+    public string BroadcasterUserId { get; set; } = string.Empty;
+    /// <summary>
+    /// The broadcaster’s login name.
+    /// </summary>
+    public string BroadcasterUserLogin { get; set; } = string.Empty;
+    /// <summary>
+    /// The broadcaster’s display name.
+    /// </summary>
+    public string BroadcasterUserName { get; set; } = string.Empty;
+    /// <summary>
+    /// Optional. User ID of moderator who approved/denied the request.
+    /// </summary>
+    public string? ModeratorIid { get; set; } 
+    /// <summary>
+    /// Optional. The moderator’s login name
+    /// </summary>
+    public string? ModeratorLogin { get; set; } 
+    /// <summary>
+    /// Optional. The moderator’s display name
+    /// </summary>
+    public string? ModeratorName { get; set; }
+    /// <summary>
+    /// User ID of user that requested to be unbanned.
+    /// </summary>
+    public string UserId { get; set; } = string.Empty;
+    /// <summary>
+    /// The user’s login name.
+    /// </summary>
+    public string UserLogin { get; set; } = string.Empty;
+    /// <summary>
+    /// The user’s display name.
+    /// </summary>
+    public string UserName { get; set; } = string.Empty;
+    /// <summary>
+    /// Optional. Resolution text supplied by the mod/broadcaster upon approval/denial of the request.
+    /// </summary>
+    public string ResolutionText { get; set; } = string.Empty;
+    /// <summary>
+    /// Dictates whether the unban request was approved or denied.
+    /// Can be the following: approved, canceled, denied
+    /// </summary>
+    public string Status { get; set; } = string.Empty;
+}
